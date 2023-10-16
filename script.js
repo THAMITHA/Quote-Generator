d7b71df36006cbc42f511676cb093ac23d17d577
Thamitha <thamitharajput@gmail.com>
2023-10-16 19:43:40 +0530

    local fetch request

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("testing");
+// let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  //Pick a Random quote from apiQuotes array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  console.log(quote);
+}
+
+//  Get Quotes From API
+// async function getQuotes() {
+//   const apiURL = "https://type.fit/api/quotes";
+
+//   try {
+//     const response = await fetch(apiURL);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {
+//     // Catch Error Here
+//   }
+// }
+
+// On Load
+// getQuotes();
+
+newQuote();
