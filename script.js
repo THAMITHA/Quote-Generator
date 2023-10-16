36765b827b64873d9bac90dab66a3ca62ef99467
Thamitha <thamitharajput@gmail.com>
2023-10-16 19:45:09 +0530

    fetch through api

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-// let apiQuotes = [];
+let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   //Pick a Random quote from apiQuotes array
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   console.log(quote);
 }
 
 //  Get Quotes From API
-// async function getQuotes() {
-//   const apiURL = "https://type.fit/api/quotes";
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
 
-//   try {
-//     const response = await fetch(apiURL);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch (error) {
-//     // Catch Error Here
-//   }
-// }
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
 
 // On Load
-// getQuotes();
-
-newQuote();
+getQuotes();
